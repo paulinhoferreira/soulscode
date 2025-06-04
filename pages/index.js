a3af02bca1dd0cf3163f7edde8bffa5dc1e26327
PAULO GERALDO FERREIRA <master.paulo@gmail.com>
2025-06-04 14:40:47 +0000

    Testando novo modelo

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,9 @@
 function Home() {
   return (
     <h1>
-      Igreja Digital "Simplificando o terreno para multiplicar o mandamento" O
-      projeto igreja digital está em desenvolvimento para atender igrejas que
-      não podem ter em seu fluxo de contas um sistema para facilitar e
-      digitalizar a burocracia e organizção que é gerada pelas demandas
-      obrigatorias. Fluxo de caixa, controle de dizmo e ofertas , contas a
-      pagar, campanhas , lista de mebros , balança de almas ganhas em
-      evangelismos, acompanhamento de novos convertidos, escala de obreiros. E
-      outras demandas que forem surgindo no decorrer da utilizção , sendo
-      disponibilizadas em novas atualizações. O projeto é totalmente gratuíto e
-      vem para ajudar a obra do Senhor. Lucas 10:2-3 "E lhes fez a seguinte
-      advertência: A seara é grande, mas os trabalhadores são poucos. Rogai,
-      pois, ao Senhor da seara que mande trabalhadores para a sua seara." "{" "}
+      Projeto Cidade Santa Digital "Simplificando o terreno para multiplicar o
+      mandamento"/ O Projeto `Cidade Santa Digital` se destina a construir um
+      sistema on-line para uso / " "{" "}
     </h1>
   );
 }
