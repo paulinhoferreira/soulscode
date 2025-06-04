a5c4f280d44828410dd02d09997a483834e2949b
PAULO GERALDO FERREIRA <master.paulo@gmail.com>
2025-06-04 15:13:25 +0000

    Missão

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,17 @@ function Home() {
   return (
     <h1>
       Projeto Cidade Santa Digital "Simplificando o terreno para multiplicar o
-      mandamento"/ O Projeto `Cidade Santa Digital` se destina a construir um
-      sistema on-line para uso / " "{" "}
+      mandamento" Paz do senhor a todos. Irmãos, o projeto Cidade Santa Digital
+      é um sonho antigo que tenho de criar um sistema para fornecer as igrejas
+      uma forma de se organizar da melhor forma possivel e entrar de vez no
+      mundo digital. Sair do controle manual , facilitando o controle
+      financeiro, de membros novos convertidos,escala de obreiros e muitos
+      outras demandas que podem surgir em novas atualizações. Tudo
+      gratuitamente, assim encurtando o tempo com a burocracia , sobrando tempo
+      para o mais importante. Ganhar Almas para o Reino. Hoje pela misericordia
+      de Deus , o Curso.Dev me deu a oportunidade de dar o primeiro passo
+      concreto, o meu espaço na internet já esta garantido. Aguarde novas
+      atualizações Deus abençoe a todos! " "{" "}
     </h1>
   );
 }
