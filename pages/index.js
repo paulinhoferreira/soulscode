c564f0d8c97b2b84a46f53bf31d0d9146df9cd4a
PAULO GERALDO FERREIRA <master.paulo@gmail.com>
2025-06-18 19:12:49 +0000

    net

---
+++ pages/index.js
@@ -0,0 +1,94 @@
+function Home() {
+  return (
+    <html lang="pt-BR">
+      <head>
+        <title>Cidade Santa Digital</title>
+      </head>
+      <header>
+        <h1>
+          <a href="#">Cidade Santa Digital </a>
+        </h1>
+      </header>
+      <body>
+        <h1>
+          <u>
+            <b>Jesus é Nosso Proposito</b>
+          </u>
+        </h1>
+        <p>
+          Irmãos estou estudando para disponibilizar a ferramenta para facilitar
+          a vida das igrejas digitalizando os processos que hoje são manuais.
+          Por enquanto vou utilizar esse espaço na internet para disponibilizar
+          devocionais diários e assim melhorando aos poucos o site conforme vou
+          evoluindo no processo. Desde já agradeço a todos e{" "}
+          <b>Glória a Deus!</b>
+        </p>
+        <hr></hr>
+        <header>
+          <h2>
+            <u>Daniel 12:4</u>
+          </h2>
+          <p>
+            Porém tu, ó querido Daniel, tranca em segredo, mediante um selo, as
+            palavras do Livro, até o tempo próprio do fim. Muitos farão de tudo
+            e correrão de uma parte a outra em busca do maior saber; e o
+            conhecimento se multiplicará muitas e muitas vezes!”
+          </p>
+        </header>
+
+        <hr></hr>
+        <h3>
+          <b>
+            A palavra foi selada pois da a entender que não era para o tempo de
+            Daniel e sim para o tempo que havia de vir. Essa busca por
+            conhecimento , que se multiplica pode ser para nosssa edificação ou
+            para nossa destruição. Temos hoje em mãos uma ferramena para busca
+            de informações,livros e videos para adquirir conhecimento. Mas
+            nenhuma dessas fontes substitui a plavra do nosso Senhor Deus, a
+            Biblia sagrada, portanto podem ser consideradas apenas fontes de
+            buscas complementares somente, que fique bem claro. Essa é forma
+            mais edificante para o uso na busca do reino.{" "}
+            <u>
+              Qualquer outra facilidade que uma IA, Youtube , Google que venham
+              trazer excluindo a busca na Biblia em primeiro lugar , deve ser
+              questionada.
+            </u>
+          </b>
+        </h3>
+        <hr></hr>
+        <h2>
+          <u>Daniel 12:10</u>
+        </h2>
+        <p>
+          Muitos serão colocados à prova, e assim serão joeirados, purificados e
+          aperfeiçoados; no entanto, os malignos continuarão praticando
+          impiedades. Nenhum dos ímpios dará ouvidos à estas palavras, tampouco
+          buscarão entendimento; mas os sábios e prudentes sim, estes
+          entenderão.
+        </p>
+        <hr></hr>
+        <h3>
+          <b>
+            A importância da busca pelo conhecimento da forma correta nos faz
+            colher os frutos na hora correta. Quem tem ouvidos que ouça, o que o
+            anjo do Senhor diz a Igreja. Somente lendo e estudando as escrituras
+            podemos entender o que temos que fazer para nos prepapar para o que
+            há de vir e como utilizar as ferramentas que a inteligência humana
+            através do espirito nos foi dada. Mas o que vemos é o homem
+            destruindo mais uma vez uma ferramenta que poderia ser utilizada
+            para o bem , para o mau, para aplicar golpes,ganhar dinheiro
+            ílicito,enganando outras pessoas. "Nenhum dos ímpios dara ouvidos a
+            essa palavra".
+          </b>
+        </h3>
+
+        <h2>https://github.com/paulinhoferreira</h2>
+        <span>Paulo Ferreira</span>
+        <br></br>
+        <span>Servo de Deus</span>
+      </body>
+    </html>
+  );
+}
+
+export default Home;
